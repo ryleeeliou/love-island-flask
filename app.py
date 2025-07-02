1997e2bafdfeae45a8749afe9404b52b3cfe4a60
Rylee Eliou <rylee.eliou@comcast.net>
2025-07-02 15:38:21 -0400

    Fix: Imported render_template into app.py, added home route and index template for the contestants

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate 
 from config import Config 
@@ -16,6 +16,16 @@ migrate = Migrate(app, db)
 #import models so they can be detected by flask
 
 from models import Contestant, Episode 
+
+@app.route("/")
+def index():
+    contestants = Contestant.query.all()
+    return render_template("index.html", contestants=contestants)
+
+@app.route("/contestant/<int:id>")
+def show_contestant(id):
+    c = Contestant.query.get_or_404(id)
+    return render_template("contestant.html", c=c)
 
 
 #routes and views here later
